badf217810cb5539a137bf3b4011370a23fe152c
junyanLiu <2509344578@qq.com>
2019-11-26 15:37:06 +0800

    添加文件

---
+++ core/main.js
@@ -0,0 +1,7 @@
+export function a() {
+    console.info('this is a');
+}
+
+export function b() {
+    console.info('this is b');
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+import { a, b } from './core/main.js';
+
+a();
+b();
